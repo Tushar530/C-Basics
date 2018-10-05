013f7d23199fa68e08c78343ca1f70654611ed53
Tushar530 <tusharsoni95@gmail.com>
2018-10-05 15:26:32 +0530

    Add files via upload

---
+++ Expression Body Expression/Manager_Expression/Manager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager_Expression
+{
+    /// <summary>
+    /// Author: Tushar Soni
+    /// DOC : 12th September
+    /// Showing Expression Bodied Function
+    ///
+    /// </summary>
+
+    public class Manager
+    {
+        public Manager(int ManagerID, string sName)
+        {
+            Manager_ID = ManagerID;
+            Name = sName;
+        }
+
+        private int Manager_ID;
+        private string Name;
+
+        public override string ToString() => $"{Manager_ID} {Name}".Trim();
+        public void DisplayName() => Console.WriteLine(ToString());
+    }
+
+    class Example
+    {
+        static void Main()
+        {
+            Manager p = new Manager(1001, "Suresh");
+         
+            p.DisplayName();
+        }
+    }
+}
+
+
+
+
