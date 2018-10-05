4569f20a347a39325d3710f72434a57495cb7992
Tushar530 <tusharsoni95@gmail.com>
2018-10-05 15:14:18 +0530

    Add files via upload

---
+++ MultiArray/MultiArray/MultArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] student = new string[,]
+            {
+                {"Tushar soni", "Narendra modi" ,"Ram Sharma" }
+
+        };
+
+            for (int i = 0; i < student.GetLength(0); i++)
+            {
+                for (int j = 0; j < student.GetLength(1); j++)
+                {
+                    Console.WriteLine(student[i, j]);
+
+                }
+            }
+        }
+    }
+}
+
